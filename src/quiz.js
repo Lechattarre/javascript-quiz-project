576061bad64ba1dbcad103c603eddd1fbc814993
john sebastian marmolejo castro <148784346+Lechattarre@users.noreply.github.com>
2024-10-21 14:55:11 +0200

    Solucion del dia uno pairpro

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,48 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
 
-    // 4. shuffleQuestions()
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+    getQuestion() {
 
-    // 5. checkAnswer(answer)
+        return this.questions[this.currentQuestionIndex];
 
-    // 6. hasEnded()
+    }
+
+    moveToNextQuestion() {
+
+        if (this.currentQuestionIndex < this.questions.length - 1) {
+
+            this.currentQuestionIndex++;
+        }
+        return this.currentQuestionIndex
+    }
+
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer) {
+
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+            this.correctAnswers += 1;
+            return true;
+        }
+        return false;
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        }
+        return true
+    }
 }
